aa9c2a6e7570b9ede32f35a2fae0e31288f35f7a
Samuel Atcheson <atcheson@usc.edu>
2025-10-09 18:38:11 -0500

    updated publication info

--- src/app/components/publications.js
+++ src/app/components/publications.js
@@ -1,20 +1,29 @@
 import React from "react";
 import publications from "../data/publications";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAward, faTrophy } from "@fortawesome/free-solid-svg-icons";
 
 export default function Publications() {    
     
     function formatAuthors({ publication }) {
         return publication.authors.map((author, index) => {
-            const formatted = index === publication.position ? (
-                <span key={index} className="font-bold">
-                {author}
-                </span>
+            const formattedBold = index === publication.position ? (
+                <span key={index} className="font-bold">{author}</span>
             ) : (
                 author
             );
 
+            const formatted = publication.equal.includes(author) ? (
+                <>
+                    {formattedBold}<sup className="text-xs">*</sup>
+                </>
+            ) : (
+                formattedBold
+            )
+
             // add comma except after last
             const separator = index < publication.authors.length - 1 ? ', ' : '';
+            
             return (
                 <React.Fragment key={index}>
                     {formatted}
@@ -26,12 +35,43 @@ export default function Publications() {
 
 
     return (
-        <ol className="list-decimal pl-4">
-          {publications.map((publication, index) => (
-            <li key={index} className="mb-8 pl-4">
-                {publication.title}. {formatAuthors({publication})}. {publication.conference} {publication.year}. <a className="text-blue-600 hover:text-blue-800 underline" href={publication.link}>[PDF]</a>
-            </li>
-          ))}
-        </ol>
+        <>
+            <ol className="list-decimal pl-4">
+            {publications.map((publication, index) => (
+                <li key={index} className="mb-8 pl-4">
+                    {publication.title}. {formatAuthors({publication})}. {publication.conference} {publication.year}.
+                    {publication.award === "medal" && (
+                        <FontAwesomeIcon 
+                            icon={faAward}
+                            size="lg"
+                            aria-hidden="true"
+                        />)
+                    }
+                    {publication.award === "trophy" && (
+                        <FontAwesomeIcon 
+                            icon={faTrophy}
+                            size="lg"
+                            aria-hidden="true"
+                        />)
+                    }
+                    {" "}<a className="text-blue-600 hover:text-blue-800 underline" href={publication.link}>[PDF]</a>
+                </li>
+            ))}
+            </ol>
+            <details>
+                <ul>
+                    <li>
+                        * = Equal contribution
+                    </li>
+                    <li>
+                        <FontAwesomeIcon
+                            icon={faAward}
+                            size="lg"
+                        />
+                        = Best Paper Honorable Mention
+                    </li>
+                </ul>
+            </details>
+        </>
     );
 }
--- src/app/data/publications.js
+++ src/app/data/publications.js
@@ -3,18 +3,20 @@ const publications = [
         "title": "\"I'd Never Actually Realized How Big An Impact It Had Until Now\": Perspectives of University Students with Disabilities on Generative Artificial Intelligence",
         "authors": ["Alex Atcheson", "Omar Khan", "Brian Siemann", "Anika Jain", "Karrie Karahalios"],
         "position": 0,
+        "equal": [],
         "conference": "CHI",
         "year": "2025",
-        "award": "medal",
+        "award": "",
         "link": "/publications/perspectives-of-university-students-with-disabilities-on-generative-artificial-intelligence.pdf"
     },
     {
         "title": "Not What it Used to Be: Characterizing Content and User-base Changes in Newly Created Online Communities",
         "authors": ["Alex Atcheson", "Vinay Koshy", "Karrie Karahalios"],
         "position": 0,
+        "equal": ["Alex Atcheson", "Vinay Koshy"],
         "conference": "CHI",
         "year": "2024",
-        "award": "",
+        "award": "medal",
         "link": "/publications/not-what-it-used-to-be.pdf"
     }
 ];
