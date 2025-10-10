d7f1ba41353df4f9b83d0a784c37bfa3c296f5ae
Samuel Atcheson <atcheson@usc.edu>
2025-10-10 11:02:15 -0500

    spelling correction

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -2,11 +2,11 @@ export default function About() {
     return (
         <>
             <p className="mb-5">
-                Hi! My name is Alex (my first name is actually Samuel, but I have always gone by Alex&mdash;it&apos;s a funny story). 
-                I am a PhD candidate in Computer Science at the University of Illinois Urbana-Champaign, where I am a member of the Social Spaces 
-                research advised by Dr. Karrie Karahalios. Prior to my time at Illinois, I earned a BS in Computer Science and Computer Engineering 
-                from the University of Southern California. While there, I was fortunate to have been mentored by Dr. Heather 
-                Culbertson in the Haptics Robotics and Virtual Interaction lab.
+                Hi! My name is Alex (my first name is actually Samuel&mdash;it&apos;s a funny story). I am a PhD candidate 
+                in Computer Science at the University of Illinois at Urbana-Champaign, where I am a member of the Social Spaces 
+                research group advised by Dr. Karrie Karahalios. Prior to my time at Illinois, I earned a BS in Computer Science 
+                and Computer Engineering from the University of Southern California. While there, I was fortunate to have been 
+                mentored by Dr. Heather Culbertson in the Haptics Robotics and Virtual Interaction lab.
             </p>
             <p className="mb-5">
                 Broadly, I am interested in problems in the area of Human-Computer Interaction, particularly as they relate to 
