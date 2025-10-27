36d49b173c234343062cd8529411d43d9d773a3e
Samuel Atcheson <atcheson@usc.edu>
2025-10-27 16:49:38 -0500

    shortened UIUC refs

--- src/app/data/courses.js
+++ src/app/data/courses.js
@@ -1,25 +1,25 @@
 const courses = [
     {
         "title": "CS 101: Introduction to Programming for Scientists and Engineers",
-        "institution": "University of Illinois at Urbana-Champaign",
+        "institution": "University of Illinois",
         "semesters": ["Fall 2025", "Fall 2024", "Fall 2022", "Spring 2022", "Fall 2021"],
         "awards": ["Fall 2021", "Fall 2022", "Fall 2024"],
     },
     {
         "title": "CS 128: Introduction to Computer Science II",
-        "institution": "University of Illinois at Urbana-Champaign",
+        "institution": "University of Illinois",
         "semesters": ["Summer 2022"],
         "awards": [],
     },
     {
         "title": "CS 416: Data Visualization",
-        "institution": "University of Illinois at Urbana-Champaign",
+        "institution": "University of Illinois",
         "semesters": ["Summer 2023"],
         "awards": [],
     },
     {
         "title": "CS 519: Scientific Visualization",
-        "institution": "University of Illinois at Urbana-Champaign",
+        "institution": "University of Illinois",
         "semesters": ["Summer 2025"],
         "awards": [],
     },
