53c5505883f80b71a820f7f99be582acdfe73bdf
Samuel Atcheson <atcheson@usc.edu>
2026-01-13 17:12:42 -0600

    updated about.js

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -3,20 +3,20 @@ export default function About() {
         <>
             <p className="mb-5">
                 Hi! My name is Alex. I am a PhD candidate 
-                in Computer Science at the University of Illinois Urbana-Champaign (UIUC), where I am advised by Dr. Karrie Karahalios in the Social Spaces Group. 
+                in Computer Science at the University of Illinois Urbana-Champaign (UIUC), where I am advised by Dr. Karrie Karahalios as a part of the Social Spaces Group. 
                 Prior to being at UIUC, I earned a BS in Computer Science 
-                and Computer Engineering from the University of Southern California (USC). While at USC, I was 
+                and Computer Engineering from the University of Southern California (USC). While there, I was 
                 mentored by Dr. Heather Culbertson as an undergraduate researcher in the Haptics Robotics and Virtual Interaction lab, where I worked on tactile, block-based programming.
             </p>
             <p className="mb-5">
                 Broadly, my research interests are in the area of Human-Computer Interaction, with focuses on 
                 Accessibility, Human-AI Interaction, and Education. In my dissertation work, I study the implications 
-                of technology, and in particular AI, for the participation of disabled persons in higher education. I both explore how 
-                existing AI tools are used by students to address unmet access needs and design human-centric systems for developing self-advocacy skills.
+                of technology, and in particular AI, for the participation of disabled persons in higher education. I explore how 
+                existing AI tools are used by students to address unmet access needs, and I design human-centric systems for supporting the development self-advocacy skills.
             </p>
             <p>
-                Beyond my dissertation work, I am also interested in problems related to information seeking, where another strand of my research 
-                examines the factors associated with different online searching behaviors. In the past, I have also investigated associations between growth and moderator activity on the development of online communities. 
+                Beyond my dissertation work, I am also interested in problems related to online information seeking polarization&mdash;this other strand of my research 
+                examines the socio-demographic factors associated with different online searching behaviors. I have also done work around understanding the relationship between growth, moderation, and the development of online communities. 
                 Outside of research, you can catch me playing soccer, scrolling through funny cat 
                 posts on Reddit, or perfecting my healthy brownie recipe (yes, it&apos;s a thing!).
             </p>
