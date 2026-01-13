ea99be0a30db8d4f842f6bd6e8ea8de18be376a6
Samuel Atcheson <atcheson@usc.edu>
2026-01-13 17:01:44 -0600

    update about.js

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -2,24 +2,23 @@ export default function About() {
     return (
         <>
             <p className="mb-5">
-                Hi! My name is Alex (my first name is actually Samuel&mdash;it&apos;s a funny story). I am a PhD candidate 
-                in Computer Science at the University of Illinois Urbana-Champaign, where I am a member of the Social Spaces 
-                research group advised by Dr. Karrie Karahalios. Prior to that, I earned a BS in Computer Science 
-                and Computer Engineering from the University of Southern California. While there, I was fortunate to have been 
-                mentored by Dr. Heather Culbertson in the Haptics Robotics and Virtual Interaction lab.
+                Hi! My name is Alex. I am a PhD candidate 
+                in Computer Science at the University of Illinois Urbana-Champaign (UIUC), where I am advised by Dr. Karrie Karahalios in the Social Spaces Group. 
+                Prior to being at UIUC, I earned a BS in Computer Science 
+                and Computer Engineering from the University of Southern California (USC). While at USC, I was 
+                mentored by Dr. Heather Culbertson as an undergraduate researcher in the Haptics Robotics and Virtual Interaction lab, where I worked on tactile, block-based programming.
             </p>
             <p className="mb-5">
-                Broadly, I am interested in problems in the area of Human-Computer Interaction, particularly as they relate to 
-                Accessibility, Human-AI Interaction, and Education. In my own work, I study the implications 
-                of AI technology for the participation of disabled persons in higher education. I both explore how 
-                existing AI tools are used by students to address access needs and design systems for improving the 
-                processes involved in creating access.
+                Broadly, my research interests are in the area of Human-Computer Interaction, with focuses on 
+                Accessibility, Human-AI Interaction, and Education. In my dissertation work, I study the implications 
+                of technology, and in particular AI, for the participation of disabled persons in higher education. I both explore how 
+                existing AI tools are used by students to address unmet access needs and design human-centric systems for developing self-advocacy skills.
             </p>
             <p>
-                I am also interested in problems related to information seeking, where another strand of my research 
-                examines the factors affecting how different people seek information online. In the past, I have also investigated growth 
-                in online communities. Outside of research, you can catch me playing soccer, scrolling through funny cat 
-                posts on Reddit, or perfecting my apple sauce recipe.
+                Beyond my dissertation work, I am also interested in problems related to information seeking, where another strand of my research 
+                examines the factors associated with different online searching behaviors. In the past, I have also investigated associations between growth and moderator activity on the development of online communities. 
+                Outside of research, you can catch me playing soccer, scrolling through funny cat 
+                posts on Reddit, or perfecting my healthy brownie recipe (yes, it&apos;s a thing!).
             </p>
         </>
     )
