3849c39cff19cd732f6280d85930b739142eb475
Samuel Atcheson <atcheson@usc.edu>
2026-03-25 16:15:59 -0500

    fixed spelling

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <>
             <p className="mb-5">
-                I am currentlya PhD candidate 
+                Hi! My name is Alex. I am currently a PhD candidate 
                 in Computer Science at the University of Illinois Urbana-Champaign (UIUC), where I am advised by Dr. Karrie Karahalios as a part of the Social Spaces Group. 
                 I earned a BS in Computer Science 
                 and Computer Engineering from the University of Southern California (USC). While at USC, I was 
